911c0f5e375f94fe77cd84630da78b47b4b84d7b
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-01-17 16:10:16 +0100

    ColorBucket: Update all our themes
    - Adapted as per DU backgrounds
    refer: https://github.com/DirtyUnicorns/android_vendor_themes/tree/q10x/backgrounds
    
    Change-Id: I36141fcb8d9871b7debccd0cbc10bd8b0277e536

--- config/common.mk
+++ config/common.mk
@@ -189,6 +189,7 @@ PRODUCT_PACKAGES += \
 # Settings
 PRODUCT_PACKAGES += \
     SystemPitchBlackOverlay \
+    SystemUIPitchBlackOverlay \
     SystemDarkGrayOverlay \
     SystemUIDarkGrayOverlay \
     SystemStyleOverlay \
