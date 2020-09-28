6649c3f6fa10c7ac05e1c66452dc0987f2f8791b
kubersharma001 <kubersharma001@gmail.com>
2020-09-28 14:46:12 +0530

    ColorBucket: A cleaner implementation of all our themes
     * Also, Rebrand PitchBlack as RavenBlack
    
    Change-Id: I1534065c81f793957fef7881f0881fc65ad2478a

--- config/common.mk
+++ config/common.mk
@@ -188,8 +188,8 @@ PRODUCT_PACKAGES += \
 # Custom Overlays
 # Settings
 PRODUCT_PACKAGES += \
-    SystemPitchBlackOverlay \
-    SystemUIPitchBlackOverlay \
+    SystemRavenBlackOverlay \
+    SystemUIRavenBlackOverlay \
     SystemDarkGrayOverlay \
     SystemUIDarkGrayOverlay \
     SystemStyleOverlay \
